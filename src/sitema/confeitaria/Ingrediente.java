05ee62915246dcdf6a64dc8124aac8d3ce59c5b7
unknown <renatalamb95@gmail.com>
2025-01-21 17:27:58 +0000

    Adicionando Classe Ingrediente

---
+++ src/sitema/confeitaria/Ingrediente.java
@@ -0,0 +1,12 @@
+package sitema.confeitaria;
+
+import java.time.LocalDate;
+
+public class Ingrediente {
+    //Atributos
+    int codIngrediente;
+    String NomeIngrediente;
+    LocalDate dataValidade;
+    int quantidade;
+
+}
