0a0c482cf669bcdc0765801f2ed9dd386c0fd9c5
True <truedbx@gmail.com>
2025-01-29 11:25:57 +0000

    Add files via upload

---
+++ Interface graphica/src/Interface graphica.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+
+class Main {
+    public static void main(String[] args) {
+        // Cria uma nova instância da classe Teste
+        new Teste();
+    }
+}
+
+class Teste {
+    // Construtor da classe Teste
+    Teste() {
+        // Cria uma nova janela JFrame com o título "Teste"
+        JFrame jframe = new JFrame("Teste");
+        jframe.setSize(800, 500); // Define o tamanho da janela
+        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Fecha o programa ao fechar a janela
+        jframe.setResizable(false); // Impede que a janela seja redimensionada
+        jframe.setLocationRelativeTo(null); // Centraliza a janela na tela
+        jframe.setVisible(true); // Torna a janela visível
+    }
+}
+
+
